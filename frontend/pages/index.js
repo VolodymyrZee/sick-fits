1c1545c00792d315e0534e3152fb6399121e1aba
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-09-09 16:06:55 -0500

    Initial commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'enzyme';
+
+class Home extends React.Component {
+    render() {
+        return <p>Hey There!</p>
+    }
+}
+
+export default Home;
