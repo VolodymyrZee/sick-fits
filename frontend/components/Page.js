e2a0ec057eff44625b1384eadcba4fafce79d199
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-09-21 19:40:46 -0500

    clickbtn

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+background: red;
+font-size: 100px;
+`;
 
  class Page extends Component {
     render() {
@@ -8,7 +14,7 @@ import Meta from './Meta';
             <div>
                 <Meta />
                <Header />
-               <button>Click Me!</button>
+               <MyButton>Click Me!</MyButton>
                 {this.props.children}
             </div>
         );
