3b525933b7f1018c07e843284eea50f501356012
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-09-29 13:39:29 -0500

    refreshing

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ea5ef3c8cde7a8081aa2";
+/******/ 	var hotCurrentHash = "fed935ef0efcdcb09762";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
