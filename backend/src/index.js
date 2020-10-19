92a8e69ee21d7cee3fd1fea86f829579a28f47db
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-10-19 16:24:14 -0500

    3m

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+//todo use experss middlware to handle cookies (JWT)
+//todo use express middleware to populate current user 
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+},
+deets => {
+    console.log('Server is now running on port https:/localhost:${deets.port}');
+}
+);
