c6bd338a2577c12265030291a496a409099d90a2
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-10-20 14:35:23 -0500

    t2

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,10 @@
 const Query = {
 
 dogs(parent, args, ctx, info) {
-    return [{name: 'Snickers'}, {name: 'Sunny'}];
+    global.dogs = global.dogs || [];
+    return global.dogs;
 
-}
+},
 
 };
 
