2ff04956d33ead6e72485b954a8fd975262befd6
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-10-29 14:41:29 -0500

    th2

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,7 +2,7 @@ const Mutations = {
    async createItem(parent, args, ctx, info) {
 //TODO: Check if they are logged in
 
-const item = await ctx.db.Mutations.createItem({
+const item = await ctx.db.mutation.createItem({
     data: {
         ...args
     }
