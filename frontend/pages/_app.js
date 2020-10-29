28c7c308e78a0b94006b740e2ee436dd50137c0a
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-10-29 16:33:45 -0500

    th1.f.e.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,34 @@
 import App, {Container} from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+    static async getInitialProps({Component, ctx}) {
+        let pageProps = {};
+        if(Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        // This exposes the query to the user
+        pageProps.query = ctx.query;
+        return {pageProps};
+    }
     render() {
+        const { Component, apollo, pageProps} = this.props;
         
         const {Component} = this.props;
         return(
 
             <Container>
+                <ApolloProvider client={this.props.apollo}>
                 <Page>
                 <Component/>
                 </Page>
+                </ApolloProvider>
             </Container>
         );
     }
 
 }
 
-export default MyApp;
+export default withData (MyApp);
